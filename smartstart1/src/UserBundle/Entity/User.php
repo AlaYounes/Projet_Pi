96bf7756b40eb2743a8ae038b063feaa493deee0
AlaYounes <47618679+AlaYounes@users.noreply.github.com>
2019-02-14 04:03:22 +0100

    FOS User Bundle By Saleh

---
+++ smartstart1/src/UserBundle/Entity/User.php
@@ -0,0 +1,32 @@
+<?php
+// src/UserBundle/Entity/User.php
+
+namespace UserBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ *
+ * Class User
+ * @package UserBundle\Entity
+ *
+ * @ORM\Entity
+ * @ORM\Table(name="fos_user")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    public function __construct()
+    {
+        parent::__construct();
+        // your own logic
+    }
+}
+
